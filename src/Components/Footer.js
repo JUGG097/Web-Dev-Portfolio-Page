14733038f8afa8100dc7d40c0b704140c0c7f0ce
Jugg1 <adeoluwaadeboye@gmail.com>
2020-11-14 20:27:10 +0100

    Done with Footer Component Design Part 1

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,18 +1,47 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Footer() {
 	return (
 		<div className="container">
-			<div className="footer fixed-bottom p-3">
+			<div className="footer fixed-bottom p-2">
 				<div className="row text-center">
 					<div className="col-12 col-sm-4">
-						<h2>Footer</h2>
+						<h5>Adeoluwa Adeboye</h5>
+						<p>Web Development | Web Deployment </p>
 					</div>
 					<div className="col-12 col-sm-4">
-						<h2>Footer</h2>
+						<h5>Quick Links</h5>
+						<Link to="/home">
+							<p>Home</p>
+						</Link>
+						<Link to="/articles">
+							<p>Articles</p>
+						</Link>
+						<Link to="/projects">
+							<p>Projects</p>
+						</Link>
 					</div>
 					<div className="col-12 col-sm-4">
-						<h2>Footer</h2>
+						<h5>Contacts</h5>
+						<a href="jj">
+							<p>Whatsapp</p>
+						</a>
+						<a href="jj">
+							<p>Twitter</p>
+						</a>
+						<a href="jj">
+							<p>LinkedIn</p>
+						</a>
+						<a href="jj">
+							<p>GitHub</p>
+						</a>
+					</div>
+				</div>
+
+				<div className="row text-center">
+					<div className="col-12">
+						<p>Copyright &copy; 2020 | Adeoluwa</p>
 					</div>
 				</div>
 			</div>
