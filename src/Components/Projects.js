37b8ccc1cc9703dd9bea79cb812962d77fc4ee4f
Jugg1 <adeoluwaadeboye@gmail.com>
2020-11-15 17:48:30 +0100

    Done with Projects page design Part 1

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,7 +1,61 @@
 import React from "react";
+import { Jumbotron } from "reactstrap";
 
 function Projects() {
-	return <div></div>;
+	return (
+		<div>
+			<Jumbotron>
+				<div className="container">
+					<h2 className="display-5">My Projects</h2>
+					<p className="lead">Projects related to Web Development</p>
+				</div>
+			</Jumbotron>
+
+			<div className="container">
+				<div className="row">
+					<div className="col-12 col-sm-4 text-center">
+						<img
+							className="img-thumbnail"
+							src="https://via.placeholder.com/200"
+							alt="PlaceHolder"
+						></img>
+					</div>
+					<div className="col-12 col-sm-8 text-center mt-2">
+						<h4>Project 1 Title</h4>
+						<p>Content</p>
+					</div>
+				</div>
+
+				<div className="row mt-3">
+					<div className="col-12 order-last col-sm-8 text-center mt-2">
+						<h4>Project 2 Title</h4>
+						<p>Content</p>
+					</div>
+					<div className="col-12 col-sm-4 text-center order-first order-sm-last">
+						<img
+							className="img-thumbnail"
+							src="https://via.placeholder.com/200"
+							alt="PlaceHolder"
+						></img>
+					</div>
+				</div>
+
+				<div className="row mt-3">
+					<div className="col-12 col-sm-4 text-center">
+						<img
+							className="img-thumbnail"
+							src="https://via.placeholder.com/200"
+							alt="PlaceHolder"
+						></img>
+					</div>
+					<div className="col-12 col-sm-8 text-center mt-2">
+						<h4>Project 1 Title</h4>
+						<p>Content</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Projects;
